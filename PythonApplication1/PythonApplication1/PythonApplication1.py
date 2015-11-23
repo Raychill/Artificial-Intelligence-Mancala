459a59ce5195a813063c57f4b3f35117ab63375e
Raecheal Howard <Howar051@cougar.csusm.edu>
2015-11-23 11:59:46 -0800

    Initial commit of python folder you can move it if you want

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,24 @@
+﻿from Ar import *
+from Domain import*
+print ("This is the beginning of your first python program rae")
+mylist = ["two", "two", "four"]
+
+#print(extractChars(mylist))
+
+var1 = extractSep(mylist)[0]#extract char
+
+print(setConstDict(mylist))
+
+x = Variable("na me",1 )
+print(x)
+for myVar in setDomainTable(extractChars(mylist)):
+    print(myVar)
+#dict1 = setConstDict(var1,mylist)#set dictionary
+
+#print(extractSep(mylist))
+
+#print(reverseWordList(mylist))
+
+#print(getMaxWordLength(mylist))
+
+#print(getColumn(3,mylist))

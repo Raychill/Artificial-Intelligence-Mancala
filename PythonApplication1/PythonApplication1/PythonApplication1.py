0bb7927dcadb6732932d3df138580c180f419db9
myon232 <myon232@gmail.com>
2015-11-26 13:27:52 -0800

    Added main and a shebang

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,24 +1,30 @@
-﻿from Ar import *
+﻿#!/usr/bin/env python3
+
+
+from Ar import *
 from Domain import*
-print ("This is the beginning of your first python program rae")
-mylist = ["two", "two", "four"]
 
-#print(extractChars(mylist))
 
-var1 = extractSep(mylist)[0]#extract char
+def main():
+    print ("This is the beginning of your first python program rae")
+    mylist = ["two", "two", "four"]
 
-print(setConstDict(mylist))
+    #print(extractChars(mylist))
 
-x = Variable("na me",1 )
-print(x)
-for myVar in setDomainTable(extractChars(mylist)):
-    print(myVar)
-#dict1 = setConstDict(var1,mylist)#set dictionary
+    var1 = extractSep(mylist)[0]#extract char
 
-#print(extractSep(mylist))
+    print(setConstDict(mylist))
 
-#print(reverseWordList(mylist))
+    x = Variable("na me",1 )
+    print(x)
+    for myVar in setDomainTable(extractChars(mylist)):
+        print(myVar)
+    #dict1 = setConstDict(var1,mylist)#set dictionary
 
-#print(getMaxWordLength(mylist))
+    #print(extractSep(mylist))
+    #print(reverseWordList(mylist))
+    #print(getMaxWordLength(mylist))
+    #print(getColumn(3,mylist))
 
-#print(getColumn(3,mylist))
+if __name__ == "__main__":
+    main()
